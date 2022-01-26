bc03f7ccfa5fcd187a7196e052945bc1b6c652c7
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2022-01-26 12:58:02 +0800

    Small updates

--- backend.sh
+++ backend.sh
@@ -1,2 +1,3 @@
+docker push nyamkamunhjin/skill-share-api
 docker ps --quiet --filter="name=skill-share-api" | xargs --no-run-if-empty docker rm -f
 docker run -dt -p 3001:3001 --restart always --name skill-share-api nyamkamunhjin/skill-share-api
